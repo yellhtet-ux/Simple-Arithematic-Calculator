e18c54c391e62b3cb371747a6dde51698e7e687b
Timmy Cole <greyhorror30@gmail.com>
2023-04-23 22:44:16 +0630

    Initial Commit

---
+++ Arithematical Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arithematical Calculator
+//
+//  Created by Timmy Cole on 4/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
